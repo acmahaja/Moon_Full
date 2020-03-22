2c2d0fd6694967af5f093633d7b3f5e70a735016
Anjaney <acmahaja@ncsu.edu>
2020-03-21 21:37:15 -0400

    Established Access to Yahoo Finance

---
+++ critical_functions.py
@@ -0,0 +1,61 @@
+import yfinance
+import GoogleNews as news
+from datetime import datetime 
+from termcolor import colored, cprint
+import string
+
+
+run = True
+
+def share(stock_val):
+    sol = yfinance.Ticker(stock_val)
+    sol_dict = sol.get_info()
+    cprint(sol_dict['shortName'],'green')
+    cprint(sol_dict['sector'], 'green')
+    cprint(sol_dict['address1'], 'green')
+    cprint(sol_dict['city'], 'green')
+    cprint(sol_dict['state'], 'green')
+    cprint(sol_dict['country'], 'green')
+
+
+def share_check(stock_val):
+    sol = yfinance.Ticker(stock_val)
+    try:
+        info = sol.get_info()
+    except:
+        return False
+    
+    if(info['shortName']):
+        return True
+    return False
+
+def main():
+    cprint("Welcome to Stock Trader", 'green')
+    while(True):
+        cprint("Ctrl+C to exit or Enter \'exit\'", 'yellow')
+        try:
+            stock =  input("Enter Stock\n")
+            stock = stock.upper()
+            if(stock == 'exit'):
+                cprint("Exiting", 'yellow')
+                break
+            elif(stock != 'exit'):
+                if not stock:
+                    cprint("Missing Value", 'red')
+                else:
+                    if not share_check(stock):
+                        cprint("Error " + stock + " not found", 'red')
+                    else:
+                        cprint("Stock exists", 'green')
+                        share(stock)
+
+
+
+        except KeyboardInterrupt:
+            cprint("Exiting", 'yellow')
+            break
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    run = True
+    main()
