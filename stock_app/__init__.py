9dceca998380ef6bce97bfca939306ea430a4167
Anjaney <acmahaja@ncsu.edu>
2020-03-24 22:57:29 -0400

    Completed basic html and passing information from homepage

--- stock_app/__init__.py
+++ stock_app/__init__.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-   return 'Hello World'
+def home():
+   return render_template('home.html')
 
-
+@app.route('/stock', methods = ['POST', 'GET'])
+def stock():
+      if request.method == 'POST':
+         result = request.form
+         return result
+ 
 if __name__ == '__main__':
    app.run()
