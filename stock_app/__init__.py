742622d869899885c86ecf1ecf6d01ed3901d851
Anjaney <acmahaja@ncsu.edu>
2020-03-31 23:38:21 -0400

    Fixed Share Check for updated API

--- stock_app/__init__.py
+++ stock_app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 
+
 from alpha_vantage.timeseries import TimeSeries
 import GoogleNews as news
 
@@ -9,21 +10,23 @@ from datetime import datetime
 
 import pandas as pd
  
- api_key = '0NWNM979O8V73XAD'
+api_key = '0NWNM979O8V73XAD'
 
 def share_check(stock_val):
-    sol = yfinance.Ticker(stock_val)
-    try:
-        info = sol.get_info()
-    except:
-        return False
-    
-    if(info['shortName']):
-        return True
-    return False
+   try:
+      ts = TimeSeries(key=api_key)
+      endpoint = ts.get_quote_endpoint(symbol=stock_val)
+   except:
+      return False
 
+   endpoint_name = endpoint[0]
+#   print(endpoint_name['01. symbol'])
+   if(endpoint_name['01. symbol'] == stock_val):
+        return True
+   return False
+
+"""
 def share_value(stock_request):
-   print(stock_request)
    sol = yfinance.Ticker(str(stock_request['stock']))
    sol_day = sol.history(period = '1d')
    ser_open = pd.Series(sol_day['Open'])
@@ -38,10 +41,9 @@ def share_value(stock_request):
    print(sol_period)
    stock_result = {'open':open_value_open,'close':open_value_close,'day_change':open_value_open-open_value_close}
    return stock_result
-   
+"""   
    
 app = Flask(__name__)
-
 @app.route('/')
 def home():
    return render_template('home.html')
@@ -51,14 +53,12 @@ def stock():
       if request.method == 'POST':
          stock_request = request.form.to_dict()
          stock_name = stock_request['stock']
-
          if(share_check(str(stock_name)) == False):
             return redirect(url_for('home'))
          else:
-            stock = share_value(stock_request)
-            stock_request.update(stock)
-            return stock_request
-            #return render_template('stock_view.html', result = share_value(stock_request))
- 
+            stock_request = share_value(stock_request)
+            return render_template('stock_view.html', result = share_value(stock_request))
+
+
 if __name__ == '__main__':
    app.run()
