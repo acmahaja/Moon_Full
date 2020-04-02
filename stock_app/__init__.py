6fcf06d3feeaa93fb2e429e0adf6f8d18bf62a85
Anjaney <acmahaja@ncsu.edu>
2020-04-02 17:52:17 -0400

    Can Read live Stock Values

--- stock_app/__init__.py
+++ stock_app/__init__.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template, request, redirect, url_for
 import yfinance as yf
 import GoogleNews as news
 
+import requests
+from bs4 import BeautifulSoup
+
 from datetime import datetime 
 from termcolor import colored, cprint
+import json
 from datetime import datetime 
 
 import pandas as pd
@@ -14,35 +18,45 @@ def share_check(stock_val):
       stock = yf.Ticker(stock_val)
    except:
       return False
-
-   if(stock is not None):
+   if(stock.info['symbol'] == stock_val):
         return True
    return False
 
-
 def share_result(stock_request):
-#   stock = yf.Ticker(string(stock_request['stock']))
-#   stock_day = stock.history(period="max")
    stock = yf.Ticker(stock_request['stock'])
    result = stock.history(period="1d")
-   result_period = stock.history(period=stock_request['period'], interval= "30m")
-   print(result_period)
+   print(stock.info)
+   result_period = stock.history(period=stock_request['period'], interval= "1h")
    difference = result['Open'][0] - result['Close'][0]
+   percentage_difference = difference / result['Open'][0]
    stock_result = {
       'stock':stock_request['stock'],
       'period':stock_request['period'],
       'open': result['Open'][0],
-      'current': result['Close'][0],
       'change': difference,
+      'percentage': round(percentage_difference,4)*100,
       'dates':result_period.index.values,
-      'Opening_overtime':result_period[['Open']].to_dict('list'),
-      'Closing_overtime':result_period[['Close']].to_dict('list')
+      'opening_overtime':result_period[['Open']].to_dict('list'),
+      'closing_overtime':result_period[['Close']].to_dict('list'),
+      'logo':stock.info['logo_url'],
+      'weblink': stock.info['website']
    }
-
    return stock_result
    
-   
+def live_share_result(stock_val):
+   url = "https://investors-exchange-iex-trading.p.rapidapi.com/stock/"+stock_val+'/book'
+   headers = {
+      'x-rapidapi-host': "investors-exchange-iex-trading.p.rapidapi.com",
+      'x-rapidapi-key': "bd07a264aemsh6a2c106dd27fc55p1cf614jsn85f08ce4e567"
+      }
+   response = requests.request("GET", url, headers=headers)
+   stock_request_dict = json.loads(response.text)
+   return str(stock_request_dict['quote']['latestPrice'])
+
+
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
    return render_template('home.html')
@@ -55,40 +69,17 @@ def stock():
          if(share_check(str(stock_name)) == False):
             return redirect(url_for('home'))
          else:
-            
             stock_data = share_result(stock_request)
-            val = stock_data['dates']
-            print(type(stock_data['Closing_overtime']['Close']))
-            return render_template('stock_view.html', title=stock_request['stock'], 
-                                       max=max(stock_data['Closing_overtime']['Close'])*1.2,  
-                                          labels=stock_data['dates'], 
-                                             values=stock_data['Closing_overtime']['Close'])
-
-@app.route("/temp")
-def chart():
-   labels = [
-      'JAN', 'FEB', 'MAR', 'APR',
-      'MAY', 'JUN', 'JUL', 'AUG',
-      'SEP', 'OCT', 'NOV', 'DEC'
-   ]
-
-   values = [
-      967.67, 1190.89, 1079.75, 1349.19,
-      2328.91, 2504.28, 2873.83, 4764.87,
-      4349.29, 6458.30, 9907, 16297
-   ]
-
-   colors = [
-      "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
-      "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
-      "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
-
-   line_labels=labels
-   line_values=values
-   return render_template('stock_view.html', title='Bitcoin Monthly Price in USD', max=17000, labels=line_labels, values=line_values)
-
-
+            return render_template('stock_view.html', 
+                                       title=stock_request['stock'],
+                                          logo = stock_data['logo'],
+                                             weblink = stock_data['weblink'],
+                                                current = live_share_result(str(stock_name)),
+                                                   percentage = stock_data['percentage'],
+                                                   difference = stock_data['change'],
+                                                         labels=stock_data['dates'], 
+                                                            values=stock_data['closing_overtime']['Close'])
 
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=5001)
+   app.run()
